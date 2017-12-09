380953b36fc09aba0573cda97c3b5be312f7554e
Paul <b10207146@mail.ntust.edu.tw>
2017-12-09 19:42:58 +0800

    add

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import Required
 from parsefb.parsefanpage import crawlfb
 import random
-from bokeh.models import (HoverTool, FactorRange, Plot, LinearAxis, Grid,Range1d)
-from bokeh.models.glyphs import VBar
-from bokeh.plotting import figure
-from bokeh.charts import Bar
-from bokeh.embed import components
-from bokeh.models.sources import ColumnDataSource
+# from bokeh.models import (HoverTool, FactorRange, Plot, LinearAxis, Grid,Range1d)
+# from bokeh.models.glyphs import VBar
+# from bokeh.plotting import figure
+# from bokeh.charts import Bar
+# from bokeh.embed import components
+# from bokeh.models.sources import ColumnDataSource
 
 
 app = Flask(__name__)
@@ -38,18 +38,18 @@ def index():
     return render_template('index.html', form=form,state=state)
 
 
-@app.route("/chart")
-def chart():
-    bars_count = 60
-    data = {"days": [], "bugs": [], "costs": []}
-    for i in range(1, bars_count + 1):
-        data['days'].append(i)
-        data['bugs'].append(random.randint(1, 100))
-        data['costs'].append(random.uniform(1.00, 1000.00))
-    hover = create_hover_tool()
-    plot = create_bar_chart(data, "Emotion ", "days","bugs", hover)
-    script, div = components(plot)
-    return render_template("chart.html", bars_count=bars_count,the_div=div, the_script=script)
+# @app.route("/chart")
+# def chart():
+#     bars_count = 60
+#     data = {"days": [], "bugs": [], "costs": []}
+#     for i in range(1, bars_count + 1):
+#         data['days'].append(i)
+#         data['bugs'].append(random.randint(1, 100))
+#         data['costs'].append(random.uniform(1.00, 1000.00))
+#     hover = create_hover_tool()
+#     plot = create_bar_chart(data, "Emotion ", "days","bugs", hover)
+#     script, div = components(plot)
+#     return render_template("chart.html", bars_count=bars_count,the_div=div, the_script=script)
 
 
 
@@ -65,38 +65,38 @@ def create_hover_tool():
     return None
 
 
-def create_bar_chart(data, title, x_name, y_name, hover_tool=None,width=1200, height=300):
-    source = ColumnDataSource(data)
-    xdr = FactorRange(factors=data[x_name])
-    ydr = Range1d(start=0, end=max(data[y_name]) * 1.2)
+# def create_bar_chart(data, title, x_name, y_name, hover_tool=None,width=1200, height=300):
+#     source = ColumnDataSource(data)
+#     xdr = FactorRange(factors=data[x_name])
+#     ydr = Range1d(start=0, end=max(data[y_name]) * 1.2)
 
-    tools = []
-    if hover_tool:
-        tools = [hover_tool, ]
+#     tools = []
+#     if hover_tool:
+#         tools = [hover_tool, ]
 
-    plot = figure(title=title, x_range=xdr, y_range=ydr, plot_width=width,
-                  plot_height=height, h_symmetry=False, v_symmetry=False,
-                  min_border=0, toolbar_location="above", tools=tools,
-                  responsive=True, outline_line_color="#666666")
+#     plot = figure(title=title, x_range=xdr, y_range=ydr, plot_width=width,
+#                   plot_height=height, h_symmetry=False, v_symmetry=False,
+#                   min_border=0, toolbar_location="above", tools=tools,
+#                   responsive=True, outline_line_color="#666666")
 
-    glyph = VBar(x=x_name, top=y_name, bottom=0, width=.8,
-                 fill_color="#e12127")
-    plot.add_glyph(source, glyph)
+#     glyph = VBar(x=x_name, top=y_name, bottom=0, width=.8,
+#                  fill_color="#e12127")
+#     plot.add_glyph(source, glyph)
 
-    xaxis = LinearAxis()
-    yaxis = LinearAxis()
+#     xaxis = LinearAxis()
+#     yaxis = LinearAxis()
 
-    plot.add_layout(Grid(dimension=0, ticker=xaxis.ticker))
-    plot.add_layout(Grid(dimension=1, ticker=yaxis.ticker))
-    plot.toolbar.logo = None
-    plot.min_border_top = 0
-    plot.xgrid.grid_line_color = None
-    plot.ygrid.grid_line_color = "#999999"
-    plot.yaxis.axis_label = "情緒指數"
-    plot.ygrid.grid_line_alpha = 0.1
-    plot.xaxis.axis_label = "距離現在天數"
-    plot.xaxis.major_label_orientation = 1
-    return plot
+#     plot.add_layout(Grid(dimension=0, ticker=xaxis.ticker))
+#     plot.add_layout(Grid(dimension=1, ticker=yaxis.ticker))
+#     plot.toolbar.logo = None
+#     plot.min_border_top = 0
+#     plot.xgrid.grid_line_color = None
+#     plot.ygrid.grid_line_color = "#999999"
+#     plot.yaxis.axis_label = "情緒指數"
+#     plot.ygrid.grid_line_alpha = 0.1
+#     plot.xaxis.axis_label = "距離現在天數"
+#     plot.xaxis.major_label_orientation = 1
+#     return plot
 
 if __name__ == "__main__":
     app.run(debug = True)
